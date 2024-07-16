bc3aa3495f6d5826916590c878c4a7de02dc66de
Chetan <cheatnchinchulkar635@gmail.com>
2024-07-17 00:28:39 +0530

    Added support to re-send the verification link to verify the account.

--- frontend/src/pages/Authentication/VerifyEmail.jsx
+++ frontend/src/pages/Authentication/VerifyEmail.jsx
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../css/Authentication/VerifyEmail.css";
 import { useLocation } from "react-router-dom";
-import { Heading, Image } from "@chakra-ui/react";
-import mailView from "../../assets/images/Email_Verify_mail.png";
+import { Heading, Image, useToast, Button } from "@chakra-ui/react";
+import mailView from "../../assets/images/Email_verify_mail_2.png";
+import { authConstants } from "../../constants/AuthConstants";
+import { Icon } from '@iconify/react';
 
 export default function VerifyEmail() {
   const location = useLocation();
+  const toast = useToast();
+  const [isLoading, setIsLoading] = useState(false);
   const email = location.state;
+
+  const handleResendVerificationLink = async () => {
+    setIsLoading(true);
+    const response = await fetch(
+      `${authConstants.dataBaseServer}/auth/resend-verification-link?email=${email}`,
+      {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          Accept: "*",
+        },
+      }
+    ).then((res) => res.json());
+
+    if (response.status == "success") {
+      toast({
+        title: `We have sent verification link on ${email}, please recheck your mail`,
+        status: "success",
+        isClosable: true,
+      });
+      setIsLoading(false);
+      return
+    }
+    toast({
+      title: `${response.message}`,
+      status: "error",
+      isClosable: true,
+    });
+    setIsLoading(false);
+  };
+
   return (
     <div className="verify_email_body">
       <div className="verify_email_container">
-        <Image boxSize="200px" src={mailView} alt="mail" />
+        <Image boxSize="350px" src={mailView} alt="mail" />
         <Heading as="h3" size="md">
-          Please Verify Your Email Address
+          Please verify your Email Address
         </Heading>
         <p>
           We have sent mail on <b>{email}</b> to verify your mail account.
@@ -21,6 +57,18 @@ export default function VerifyEmail() {
           Please verify this email adddress by clicking on provided <b>link</b>{" "}
           in sent mail
         </p>
+        {email ? (
+          <Button
+            isLoading={isLoading}
+            onClick={() => handleResendVerificationLink()}
+            colorScheme="teal"
+            size="lg"
+          >
+            <p><Icon icon="mdi:email-resend" /></p> &nbsp; <span>Re-send link</span>
+          </Button>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
